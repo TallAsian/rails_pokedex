3690d3c1ff6b7fb67efd1d3c8dc1e24eb3b54dac
TallAsian <elijah.rohi@gmail.com>
2023-10-21 16:21:00 -0500

    Added some css

--- app/controllers/pokemon_infos_controller.rb
+++ app/controllers/pokemon_infos_controller.rb
@@ -1,7 +1,9 @@
 class PokemonInfosController < ApplicationController
   def index
+    @pokemon_infos = PokemonInfo.includes(:pokemon_gen).all
   end
 
   def show
+    @pokemon_info = PokemonInfo.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
-  get 'pokemon_stats/index'
-  get 'pokemon_stats/show'
-  get 'type_names/index'
-  get 'type_names/show'
-  get 'pokemon_ranks/index'
-  get 'pokemon_ranks/show'
-  get 'pokemon_gens/index'
-  get 'pokemon_gens/show'
-  get 'pokemon_types/index'
-  get 'pokemon_types/show'
-  get 'pokemon_infos/index'
-  get 'pokemon_infos/show'
+  # get 'pokemon_stats/index'
+  # get 'pokemon_stats/show'
+  # get 'type_names/index'
+  # get 'type_names/show'
+  # get 'pokemon_ranks/index'
+  # get 'pokemon_ranks/show'
+  # get 'pokemon_gens/index'
+  # get 'pokemon_gens/show'
+  # get 'pokemon_types/index'
+  # get 'pokemon_types/show'
+  # get 'pokemon_infos/index'
+  # get 'pokemon_infos/show'
+
+resources :pokemon_infos, only: [:index, :show]
+resources :pokemon_types, only: [:index, :show]
+resources :pokemon_stats, only: [:index, :show]
+resources :pokemon_gens, only: [:index, :show]
+resources :pokemon_ranks, only: [:index, :show]
+resources :type_names, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
