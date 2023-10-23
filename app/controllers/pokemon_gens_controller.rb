c8169ce868bdb861755ee492ef06e3c412a12c63
TallAsian <elijah.rohi@gmail.com>
2023-10-23 15:24:21 -0500

    Added generations part

--- app/controllers/pokemon_gens_controller.rb
+++ app/controllers/pokemon_gens_controller.rb
@@ -1,7 +1,10 @@
 class PokemonGensController < ApplicationController
   def index
+    @pokemon_gen = PokemonGen.all
   end
 
   def show
+    @pokemon_infos = PokemonInfo.all
+    @pokemon_gen = PokemonGen.find(params[:id])
   end
 end
