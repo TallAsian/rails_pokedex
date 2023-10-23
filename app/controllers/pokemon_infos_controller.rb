a7dc76abcd45f0ef1abb16a2d140a8a7afd793fa
TallAsian <elijah.rohi@gmail.com>
2023-10-23 15:02:23 -0500

    Added ranks

--- app/controllers/pokemon_infos_controller.rb
+++ app/controllers/pokemon_infos_controller.rb
@@ -7,5 +7,6 @@ class PokemonInfosController < ApplicationController
     @pokemon_info = PokemonInfo.find(params[:id])
     @pokemon_gen = PokemonGen.find_by(id: @pokemon_info.pokemon_gen_id)
     @pokemon_rank = PokemonRank.find_by(id: @pokemon_info.pokemon_rank_id)
+    @pokemon_type = PokemonType.all
   end
 end
--- app/controllers/pokemon_ranks_controller.rb
+++ app/controllers/pokemon_ranks_controller.rb
@@ -1,7 +1,10 @@
 class PokemonRanksController < ApplicationController
   def index
+    @pokemon_rank = PokemonRank.all
   end
 
   def show
+    @pokemon_infos = PokemonInfo.all
+    @pokemon_rank = PokemonRank.find(params[:id])
   end
 end
