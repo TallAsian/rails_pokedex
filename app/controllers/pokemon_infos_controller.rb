9c8127e3829968a549585770527d570560a11f67
TallAsian <elijah.rohi@gmail.com>
2023-10-23 09:55:54 -0500

    added pokemon gen, rank and desc to each pokemon

--- app/controllers/pokemon_infos_controller.rb
+++ app/controllers/pokemon_infos_controller.rb
@@ -1,9 +1,11 @@
 class PokemonInfosController < ApplicationController
   def index
-    @pokemon_infos = PokemonInfo.includes(:pokemon_gen).all
+    @pokemon_infos = PokemonInfo.all
   end
 
   def show
     @pokemon_info = PokemonInfo.find(params[:id])
+    @pokemon_gen = PokemonGen.find_by(id: @pokemon_info.pokemon_gen_id)
+    @pokemon_rank = PokemonRank.find_by(id: @pokemon_info.pokemon_rank_id)
   end
 end
